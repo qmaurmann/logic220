ea8472a7dc4a14a8cad91fbdf5911d41f69128d1
Quinn Maurmann <quinn.maurmann@gmail.com>
2015-04-12 17:22:26 -0700

    Add more example functions, add QuickCheck tests (testing prPred revealed a bug in interp!)

--- PrimRec.hs
+++ PrimRec.hs
@@ -21,7 +21,7 @@ interp pr args = case pr of
     PrComp f gs -> interp f $ fmap (\g -> interp g args) gs
     PrRDef f g -> h args where
         h (VCons x xs) = if x == 0 then interp f xs else -- TODO fail-fast negatives?
-            let r = h (VCons (x-1) xs) in interp g (VCons r (VCons x xs))
+            let r = h (VCons (x-1) xs) in interp g (VCons r (VCons (x-1) xs))
 
 interpChecked :: (Num a, Ord a, Show a) => (PrimRec n) -> Vect n a -> a
 interpChecked pr = interp pr . negCheck
@@ -29,7 +29,7 @@ interpChecked pr = interp pr . negCheck
 negCheck :: (Num a, Ord a, Show a) => Vect n a -> Vect n a
 negCheck xs = if vand (>0) xs then xs else error $ "negCheck failed for " ++ show xs
 
--- Useful abbreviations, polymorphic in type
+-- Useful abbreviations, for projections, polymorphic in type
 pi0 = PrProj FZ   :: PrimRec (TS n)
 pi1 = PrProj fin1 :: PrimRec (TS (TS n))
 pi2 = PrProj fin2 :: PrimRec (TS (TS (TS n)))
@@ -37,18 +37,62 @@ pi3 = PrProj fin3 :: PrimRec (TS (TS (TS (TS n))))
 pi4 = PrProj fin4 :: PrimRec (TS (TS (TS (TS (TS n)))))
 pi5 = PrProj fin5 :: PrimRec (TS (TS (TS (TS (TS (TS n))))))
 
+-- Useful helpers
 
--- Example functions:
+mkComp1 :: PrimRec T1 -> PrimRec n -> PrimRec n
+mkComp1 f g = PrComp f $ mkVect1 g
+
+mkComp2 :: PrimRec T2 -> PrimRec n -> PrimRec n -> PrimRec n
+mkComp2 f g1 g2 = PrComp f $ mkVect2 g1 g2
+
+mkFlip :: PrimRec T2 -> PrimRec T2
+mkFlip f = mkComp2 f pi1 pi0
+
+-- Example functions (see Lemma 3I.7 of Moschovakis):
+
 prId :: PrimRec T1
 prId = pi0
 
 prPlus :: PrimRec T2
-prPlus = PrRDef prId $ PrComp PrSucc $ mkVect1 pi0
+prPlus = PrRDef prId $ mkComp1 PrSucc pi0
 
-prConstZ :: PrimRec n
-prConstZ = PrComp PrZero VNil
+-- returns 0 for any args; polymorphic in number of args
+prConst0 :: PrimRec n
+prConst0 = PrComp PrZero VNil
+
+prConst1 :: PrimRec n
+prConst1 = mkComp1 PrSucc prConst0
 
 prMult :: PrimRec T2
-prMult = PrRDef prConstZ $ PrComp prPlus $ mkVect2 pi2 pi0
+prMult = PrRDef prConst0 $ mkComp2 prPlus pi2 pi0
 
+prFact :: PrimRec T1
+prFact = PrRDef prConst1 $ mkComp2 prMult (mkComp1 PrSucc pi1) pi0
 
+-- predecessor (or 0)
+prPred :: PrimRec T1
+prPred = PrRDef PrZero pi1
+
+-- monus x y = max(x-y, 0)
+prMonus :: PrimRec T2
+prMonus = mkFlip $ PrRDef prId $ mkComp1 prPred pi0
+
+-- max x y = (monus x y) + y
+prMax :: PrimRec T2
+prMax = mkComp2 prPlus prMonus pi1
+
+-- min x y = x `monus` (x `monus` y)
+prMin :: PrimRec T2
+prMin = mkComp2 prMonus pi0 prMonus
+
+-- Skipping n-ary min/max for now. There would be no unified formula, so would
+-- probably want some way of folding over args?
+
+prSymmDiff :: PrimRec T2
+prSymmDiff = mkComp2 prPlus prMonus (mkFlip prMonus)
+
+prBit :: PrimRec T1
+prBit = PrRDef PrZero prConst1
+
+prNegBit :: PrimRec T1
+prNegBit = PrRDef prConst1 prConst0
---
+++ PrimRecTests.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module PrimRecTests where
+
+import Test.QuickCheck
+import Test.QuickCheck.All
+import Vect
+import PrimRec
+
+prop_prId_correct (NonNegative x) =
+    interp prId (mkVect1 x) == x
+
+prop_prPlus_correct (NonNegative x, NonNegative y) =
+    interp prPlus (mkVect2 x y) == x + y
+
+prop_prConst0_0_correct =
+    interp prConst0 VNil == 0
+prop_prConst0_1_correct (NonNegative x) =
+    interp prConst0 (mkVect1 x) == 0
+prop_prConst0_2_correct (NonNegative x, NonNegative y) =
+    interp prConst0 (mkVect2 x y) == 0
+
+prop_prMult_correct (NonNegative x, NonNegative y) =
+    interp prMult (mkVect2 x y) == x * y
+
+--prFact is very slow without native + and *, so only check it up to 8
+prop_prFact_smallNumbers =
+    [interp prFact (mkVect1 x) | x <- [0..8]] == [product [1..x] | x <- [0..8]]
+
+prop_prPred_correct (NonNegative x) =
+    interp prPred (mkVect1 x) == max 0 (x-1)
+
+prop_prMonus_correct (NonNegative x, NonNegative y) =
+    interp prMonus (mkVect2 x y) == max 0 (x-y)
+
+prop_prMin_correct (NonNegative x, NonNegative y) =
+    interp prMin (mkVect2 x y) == min x y
+
+prop_prMax_correct (NonNegative x, NonNegative y) =
+    interp prMax (mkVect2 x y) == max x y
+
+prop_prSymmDiff_correct (NonNegative x, NonNegative y) =
+    interp prSymmDiff (mkVect2 x y) == abs (x-y)
+
+prop_prBit_correct (NonNegative x) =
+    interp prBit (mkVect1 x) == if x == 0 then 0 else 1
+
+prop_prNegBit_correct (NonNegative x) =
+    interp prNegBit (mkVect1 x) == if x == 0 then 1 else 0
+
+
+runTests = $quickCheckAll
